1f1ce322cb7e16cc0f6f4c5ce6b262df3982c4fc
Brian <bperin42@gmail.com>
2021-10-03 22:24:33 -0700

    hello world

--- src/main/kotlin/com/fast/api/controller/Hello.kt
+++ src/main/kotlin/com/fast/api/controller/Hello.kt
@@ -1,13 +1,17 @@
 package com.fast.api.controller
 
+
+import com.google.gson.JsonObject
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api/v1/healthcheck")
 class Hello {
 
-    @GetMapping("healthcheck")
-    fun sayHello(): String {
-        return "hello world we're up"
+    @GetMapping("/")
+    fun sayHello() {
+        return JsonObject().addProperty("good", "to go")
     }
 }
