ece0446af9894a7bca7b89e38319100c09f0dee8
Brian <bperin42@gmail.com>
2021-10-03 20:18:32 -0700

    add constants some are likely used before

---
+++ src/main/kotlin/com/fast/api/util/Constants.kt
@@ -0,0 +1,28 @@
+package com.fast.api.util
+
+object Constants {
+
+    const val USER = "user"
+    const val USERNAME = "username"
+    const val ALREADY_IN_USE = "already in use"
+    const val EMAIL = "already in use"
+    const val CLAIMS = "claims"
+    const val ROLE = "role"
+    const val ROLES = "roles"
+    const val EVENT = "event"
+    const val USER_ID = "userId"
+    const val JWT_TOKEN_HEADER = "X-Auth-Token"
+    const val JWT_TOKEN_EXPIRATION_HEADER = "X-Auth-Expiration"
+    const val JWT_REFRESH_TOKEN_HEADER = "X-Refresh-Token"
+    const val JWT_REFRESH_TOKEN_EXPIRATION_HEADER = "X-Refresh-Expiration"
+    const val CLIENT_IP = "X-Forwarded-For"
+    const val LAT = "lat"
+    const val LNG = "lng"
+    const val PRIVATE = "private"
+    const val MOBILE = "MOBILE"
+    const val AUTHORIZATION = "authorization"
+    const val BEARER = "Bearer"
+    const val APPLICATION_JSON = "application/json"
+
+
+}
