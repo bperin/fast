2597dc5fd9edaee00cbdaca36aa59d13b4bb59f2
Brian <bperin42@gmail.com>
2021-10-03 22:24:56 -0700

    cant spell

--- src/main/kotlin/com/fast/api/controller/Hello.kt
+++ src/main/kotlin/com/fast/api/controller/Hello.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/healthcheck")
+@RequestMapping("/api/v1/healthcare")
 class Hello {
 
     @GetMapping("/")
