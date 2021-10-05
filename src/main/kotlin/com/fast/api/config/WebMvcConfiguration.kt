b1a0831d946ca7204ed818199cdd29c7422cd9e6
Brian <bperin42@gmail.com>
2021-10-04 21:22:40 -0700

    output json for everything

---
+++ src/main/kotlin/com/fast/api/config/WebMvcConfiguration.kt
@@ -0,0 +1,15 @@
+package com.fast.api.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebMvcConfiguration : WebMvcConfigurer {
+    override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON)
+    }
+}
