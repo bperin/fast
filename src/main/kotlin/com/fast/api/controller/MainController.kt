575ab54bbbfaae390eb86bf991797abb0ec7cd81
Brian <bperin42@gmail.com>
2021-11-01 20:02:43 -0700

    singleton to handle current user

--- src/main/kotlin/com/fast/api/controller/MainController.kt
+++ src/main/kotlin/com/fast/api/controller/MainController.kt
@@ -1,6 +1,15 @@
 package com.fast.api.controller
 
+import com.fast.api.repo.UsersRepo
+import com.fast.api.security.TokenService
+import com.fast.api.security.TokenUtil
+import com.fast.api.util.UserUtil
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import javax.servlet.http.HttpServletRequest
 
 /**
  * Put common logic between controllers here
@@ -8,4 +17,29 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 open class MainController {
 
+    @Autowired
+    lateinit var tokenService: TokenService
+
+    @Autowired
+    lateinit var usersRepo: UsersRepo
+
+    /**
+     * If token is present extract subject and find the user
+     * store in our UserUtil singleton
+     */
+    @ModelAttribute("user")
+    fun findUser(request: HttpServletRequest) {
+
+        TokenUtil.extractToken(request)?.let { token ->
+
+            val claims = tokenService.getClaims(token)
+            val id = claims.subject
+
+            usersRepo.findByIdOrNull(UUID.fromString(id))?.let {
+                UserUtil.user = it
+            } ?: kotlin.run {
+                println("user not found")
+            }
+        }
+    }
 }
---
+++ src/main/kotlin/com/fast/api/util/UserUtil.kt
@@ -0,0 +1,10 @@
+package com.fast.api.util
+
+import com.fast.api.model.User
+
+/**
+ * Hold static reference to our current user
+ */
+object UserUtil {
+    var user: User? = null
+}
