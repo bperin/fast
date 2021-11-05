70ca9710305778efeee8ac9193e28c5f0f625e3c
Brian <bperin42@gmail.com>
2021-11-04 22:58:44 -0700

    rename class

--- src/main/kotlin/com/fast/api/service/LoadApplication.kt
+++ src/main/kotlin/com/fast/api/service/LoadApplication.kt
@@ -2,13 +2,15 @@ package com.fast.api.service
 
 import com.fast.api.model.Movie
 import com.fast.api.repo.MoviesRepo
+import com.fast.api.repo.UsersRepo
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.event.ContextRefreshedEvent
 import org.springframework.context.event.EventListener
 import org.springframework.core.io.ClassPathResource
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import java.lang.reflect.Type
 import java.nio.file.Files
 
@@ -17,19 +19,33 @@ import java.nio.file.Files
  * On an app start check if we have any movies in the db
  * if not add the ones from the json list ideally a migration
  * script would be best
+ * Also create the primary owner from env vars
  */
-@Component
-class LoadMoviesService {
+@Service
+class LoadApplication {
 
     @Autowired
     lateinit var moviesRepo: MoviesRepo
 
     @Autowired
+    lateinit var usersRepo: UsersRepo
+
+    @Autowired
     lateinit var imdbService: ImdbService
+
+    @Value("\${primary.owner.email}")
+    lateinit var primaryOwnerEmail: String
+
+
+    @Value("\${primary.owner.password}")
+    lateinit var primaryOwnerPassword: String
 
     @EventListener
     fun onApplicationEvent(event: ContextRefreshedEvent?) {
 
+        if (usersRepo.findAll().toList().isNullOrEmpty()) {
+
+        }
         if (moviesRepo.findAll().toList().isNullOrEmpty()) {
 
             moviesRepo.deleteAll()
